31bc7c805d5cd1ab97eaed0434d8595d2ca311cb
Dharshini <dharshinipandian2005@gmail.com>
2025-01-23 09:33:47 +0530

    new commit

--- Backend/models/db.js
+++ Backend/models/db.js
@@ -1,3 +1,33 @@
+// import pkg from 'pg';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const { Client } = pkg;
+
+// // Database configuration
+// const db = new Client({
+//     user: process.env.DB_USER,
+//     host: process.env.PG_HOST,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.PG_PORT,
+//     ssl: {
+//         rejectUnauthorized: false, // Required for Render's SSL setup
+//     },
+// });
+
+// // Connect to the database
+// db.connect((err) => {
+//     if (err) {
+//         console.error('Database connection failed:', err.stack);
+//         return;
+//     }
+//     console.log('Connected to database.');
+// });
+
+// export default db;
+
 import pkg from 'pg';
 import dotenv from 'dotenv';
 
@@ -7,11 +37,7 @@ const { Client } = pkg;
 
 // Database configuration
 const db = new Client({
-    user: process.env.DB_USER,
-    host: process.env.PG_HOST,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.PG_PORT,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false, // Required for Render's SSL setup
     },
